7691acc120fc1f641136493638ba4e4b03c3079d
Im Yongsik <lvhi7121@gmail.com>
2022-04-04 19:54:10 +0900

    feat: pch 추가
    헤더 파일들에서 intellisence가 정상적으로 작동을 안해서 squizzles 옵션을 그냥 껐습니다 ㅠㅠ

--- Kepler/Core/EntryPoint.h
+++ Kepler/Core/EntryPoint.h
@@ -9,15 +9,12 @@ int main(int argc, char** argv)
 	printf("Kepler Engine\n");
 	kepler::Log::Init();
 	
-	KEPLER_CORE_WARNING("Initializing Log System");
 	int year = 2022;
 	int month = 4;
 	int day = 4;
+	KEPLER_CORE_WARNING("Initializing Log System");
 	KEPLER_CORE_INFO("Welcome to Kepler Engine, this code written in {0}/{1:02}/{2:02}", year, month, day);
 	
-	//kepler::Log::GetCoreLogger()->warn("Initialized Log");
-	//kepler::Log::GetClientLogger()->info("Initialized Log");
-
 	auto app = kepler::CreateApplication();
 	app->Run();
 	delete app;
