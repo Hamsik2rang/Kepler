da23e648cacdb8eb54fac339248ddd1ed94330da
Im Yongsik <lvhi7121@gmail.com>
2022-04-06 17:42:44 +0900

    fix: 클라이언트 로거 잘못 선언된 수정 및 주석 추가
    클라이언트 로거를 코어 로거로 잘못 선언하여 수정합니다. 또한 로그 출력 매크로를 위험 강도에 따라 재배치하고 주석을 추가했습니다.

--- Kepler/Core/Log.h
+++ Kepler/Core/Log.h
@@ -25,16 +25,21 @@ namespace kepler {
 }
 
 // Core Log macros
-#define KEPLER_CORE_ERROR(...)		::kepler::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define KEPLER_CORE_WARNING(...)	::kepler::Log::GetCoreLogger()->warn(__VA_ARGS__)
+// 엔진(코어) 단에서 발생하는 이슈들에 대한 로그 출력에 사용합니다.
+// 아래로 갈수록 (위험)강도가 높습니다.
 #define KEPLER_CORE_INFO(...)		::kepler::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define KEPLER_CORE_TRACE(...)		::kepler::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define KEPLER_CORE_WARNING(...)	::kepler::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define KEPLER_CORE_ERROR(...)		::kepler::Log::GetCoreLogger()->error(__VA_ARGS__)
 #define KEPLER_CORE_CRITICAL(...)	::kepler::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
+
 // Client Log macros
-#define KEPLER_ERROR(...)	::kepler::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define KEPLER_WARNING(...)	::kepler::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define KEPLER_INFO(...)		::kepler::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define KEPLER_TRACE(...)	::kepler::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define KEPLER_CRITICAL(...)	::kepler::Log::GetCoreLogger()->critical(__VA_ARGS__)
+// 클라이언트 단에서 발생하는 이슈들에 대한 로그 출력에 사용합니다.
+// 아래로 갈수록 (위험)강도가 높습니다.
+#define KEPLER_INFO(...)			::kepler::Log::GetClientLogger()->info(__VA_ARGS__)
+#define KEPLER_TRACE(...)			::kepler::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define KEPLER_WARNING(...)			::kepler::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define KEPLER_ERROR(...)			::kepler::Log::GetClientLogger()->error(__VA_ARGS__)
+#define KEPLER_CRITICAL(...)		::kepler::Log::GetClientLogger()->critical(__VA_ARGS__)
 
