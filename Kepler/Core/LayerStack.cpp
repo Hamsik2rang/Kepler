560e17a114152f9d24263e601f5c04029005557e
Im Yongsik <lvhi7121@gmail.com>
2022-04-11 16:41:51 +0900

    refactor: LayerStack vector기반->list기반으로 수정
    어차피 top layer iterator를 기준으로만 push/pop을 진행하기 때문에 삽입/삭제 시간복잡도가 더 빠른 Linked List 구조로 수정했습니다.

--- Kepler/Core/LayerStack.cpp
+++ Kepler/Core/LayerStack.cpp
@@ -6,21 +6,30 @@ namespace kepler {
 
 	LayerStack::LayerStack()
 	{
-		m_lastPushedLayer = m_pLayers.begin();
+		m_lastLayerIterator = m_pLayers.begin();
 	}
 
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_pLayers)
 		{
-			delete layer;
+			if (layer)
+			{
+				delete layer;
+				layer = nullptr;
+			}
 		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
 	{
 		// 일반 Layer는 스택처럼 쌓여야 함
-		m_lastPushedLayer = m_pLayers.emplace(m_lastPushedLayer, layer);
+		//m_pLayers.emplace(m_pLayers.begin() + m_lastLayerIndex, layer);
+		if (!m_pLayers.empty())
+		{
+			m_lastLayerIterator++;
+		}
+		m_lastLayerIterator = m_pLayers.emplace(m_lastLayerIterator, layer);
 	}
 
 	void LayerStack::PushOverlay(Layer* overlay)
@@ -33,18 +42,18 @@ namespace kepler {
 	{
 		auto it = std::find(m_pLayers.begin(), m_pLayers.end(), layer);
 		// 인자로 받은 찾고자 하는 레이어가 레이어 스택 안에 존재하는 경우
-		if (it != m_pLayers.end())
+		if (it != m_pLayers.end())	
 		{
 			// 해당 레이어를 제거하고 레이어 스택의 오버레이를 제외한 top index를 한칸 내림(Stack의 pop과 동일)
 			m_pLayers.erase(it);
-			m_lastPushedLayer--;
+			m_lastLayerIterator--;
 		}
 	}
 
 	void LayerStack::PopOverlay(Layer* overlay)
 	{
 		// 모든 오버레이는 다른 어떤 일반 레이어보다도 위에 있으므로 lastPushedLayer부터 찾으면 됨
-		auto it = std::find(m_lastPushedLayer, m_pLayers.end(), overlay);
+		auto it = std::find(m_lastLayerIterator, m_pLayers.end(), overlay);
 		// 인자로 받은 오버레이가 스택 안에 존재하는 경우
 		if (it != m_pLayers.end())
 		{
--- Kepler/Core/LayerStack.h
+++ Kepler/Core/LayerStack.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <vector>
+#include <list>
 
 #include "Core/Layer.hpp"
 
@@ -9,21 +9,21 @@ namespace kepler {
 	class KEPLER_API LayerStack
 	{
 	private:
-		std::vector<Layer*> m_pLayers;
-		std::vector<Layer*>::iterator m_lastPushedLayer;
-
+		std::list<Layer*>			m_pLayers;
+		std::list<Layer*>::iterator m_lastLayerIterator;
 	public:
 		LayerStack();
 		~LayerStack();
 
+		// NOTICE: Layer, Overlay Push 시 new 키워드를 이용해 생성자를 직접 전달해야 합니다!
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* overlay);
 		void PopLayer(Layer* layer);
 		void PopOverlay(Layer* overlay);
 		
 		// LayerStack 순회를 위한 Iterator 구현
-		inline std::vector<Layer*>::iterator begin() { return m_pLayers.begin(); }
-		inline std::vector<Layer*>::iterator end() { return m_pLayers.end(); }
+		inline std::list<Layer*>::iterator begin() { return m_pLayers.begin(); }
+		inline std::list<Layer*>::iterator end() { return m_pLayers.end(); }
 	};
 
 }
