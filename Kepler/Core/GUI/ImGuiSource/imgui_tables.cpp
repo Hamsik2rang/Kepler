f98dbcbada75746f15862a38ea30138e417e3fa4
Im Yongsik <lvhi7121@gmail.com>
2022-04-12 10:02:01 +0900

    feat: ImGui(submodule) docking 브랜치로 변경

--- Kepler/Core/GUI/ImGuiSource/imgui_tables.cpp
+++ Kepler/Core/GUI/ImGuiSource/imgui_tables.cpp
@@ -183,11 +183,11 @@ Index of this file:
 //-----------------------------------------------------------------------------
 // [SECTION] Header mess
 //-----------------------------------------------------------------------------
-#include "kepch.h"
 
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
+#include "kepch.h"
 
 #include "imgui.h"
 #ifndef IMGUI_DISABLE
