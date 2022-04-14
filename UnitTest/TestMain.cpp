bb31b1e0feb025441ecd7f17717ed18e136438e3
Im Yongsik <lvhi7121@gmail.com>
2022-04-14 16:44:09 +0900

    stye: InputManager 테스트 코드들 헤더 단위로 분리

--- UnitTest/TestMain.cpp
+++ UnitTest/TestMain.cpp
@@ -1,23 +1,14 @@
 #include "kepch.h"
 #include "pch.h"
 
-#include "Core/Input.h"
+#include "InputManagerTest.h"
 
-TEST(TestCaseName, TestName)
-{
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-}
-
-TEST(KeplerButtonNotDown, KeplerInputTest)
-{
-	EXPECT_FALSE(kepler::Input::IsButtonDown(1));
-}
-
-TEST(KeplerButtonDown, KeplerInputTest)
-{
-	EXPECT_TRUE(kepler::Input::IsButtonDown(1));
-}
+//
+//TEST(TestCaseName, TestName)
+//{
+//	EXPECT_EQ(1, 1);
+//	EXPECT_TRUE(true);
+//}
 
 int main(int argc, char** argv)
 {
