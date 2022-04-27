a9f983dc500b141f7a43283fb3805bb57d192caa
Im Yongsik <lvhi7121@gmail.com>
2022-04-27 10:29:02 +0900

    feat: pch에 DirectXTK 라이브러리 추가

--- Kepler/kepch.h
+++ Kepler/kepch.h
@@ -34,3 +34,24 @@
 #pragma comment(lib, "d3d11.lib")
 #include <d3d11.h>
 #endif
+
+// DirectXTK
+#include "BufferHelpers.h"
+#include "CommonStates.h"
+#include "DDSTextureLoader.h"
+#include "DirectXHelpers.h"
+#include "Effects.h"
+#include "GamePad.h"
+#include "GeometricPrimitive.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Model.h"
+#include "Mouse.h"
+#include "PostProcess.h"
+#include "PrimitiveBatch.h"
+#include "ScreenGrab.h"
+#include "SimpleMath.h"
+#include "SpriteBatch.h"
+#include "SpriteFont.h"
+#include "VertexTypes.h"
+#include "WICTextureLoader.h"
