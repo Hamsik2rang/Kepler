42970d55c81dd17b386c818266a3ef62e27bfb29
Im Yongsik <lvhi7121@gmail.com>
2022-05-02 11:44:53 +0900

    fix: 매크로에 달린 세미콜론 제거

--- Kepler/Core/Base.h
+++ Kepler/Core/Base.h
@@ -9,7 +9,7 @@
 #define BIT_UINT8(x)	(static_cast<uint8_t>(1) << (x))
 #define BIT_INT16(x)	(static_cast<int16_t>(1) << (x))
 #define BIT_UINT16(x)	(static_cast<uint16_t>(1u) << (x))
-#define BIT_INT32(x)	(static_cast<int32_t>(1) << (x));
+#define BIT_INT32(x)	(static_cast<int32_t>(1) << (x))
 #define BIT_UINT32(x)	(static_cast<uint32_t>(1u) << (x))
 #define BIT_INT64(x)	(static_cast<int64_t>(1) << (x))
 #define BIT_UINT64(x)	(static_cast<uint64_t>(1u) << (x))
