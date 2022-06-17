1bfcdd12f0f61ee638b3c3154e16cc9d01fea1d5
Im Yongsik <lvhi7121@gmail.com>
2022-06-17 17:00:42 +0900

    style: 프레임체크 디버그 문구 수정

--- Client/Sources/GameLayer.cpp
+++ Client/Sources/GameLayer.cpp
@@ -58,7 +58,6 @@ void GameLayer::OnUpdate(float deltaTime)
 {
 	m_time += deltaTime;
 
-
 	if (kepler::Input::IsButtonDown(kepler::key::Escape))
 	{
 		kepler::Application::Get()->Shutdown();
@@ -333,7 +332,7 @@ void GameLayer::OnGUIRender()
 	ImGuiIO& io = ImGui::GetIO();
 
 	ImGui::Begin("Debug Mode");
-	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+	ImGui::Text("Frame : %.3f ms (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
 	ImGui::NewLine();
 	ImGui::Text("Press R to Respaw Ball");
 	ImGui::NewLine();
