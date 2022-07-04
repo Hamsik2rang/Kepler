7b7ee329d1acda4a9a6d2aa853169a48b6baa0e9
Im Yongsik <lvhi7121@gmail.com>
2022-07-04 23:18:41 +0900

    feat: Render State enum type 추가

--- Kepler/Renderer/RenderState.h
+++ Kepler/Renderer/RenderState.h
@@ -3,8 +3,51 @@
 #include <memory>
 
 #include "Core/Base.h"
+#include "Shader.h"
 
 namespace kepler {
+
+	enum class ePrimitiveTopology
+	{
+		None = 0,
+		Point,
+		Line,
+		Triangle,
+		LineStrip,
+		TriangleStrip,
+	};
+
+	enum class eDepthComparer
+	{
+		None = 0,
+		Never,
+		NotEqual,
+		Less,
+		LessOrEqual,
+		Equal,
+		Greater,
+		GreaterOrEqual,
+		Always,
+	};
+
+	struct RenderStateDescription
+	{
+		std::shared_ptr<IShader> pVertexShader;
+		std::shared_ptr<IShader> pHullShader;
+		std::shared_ptr<IShader> pDomainShader;
+		std::shared_ptr<IShader> pGeometryShader;
+		std::shared_ptr<IShader> pPixelShader;
+		
+		ePrimitiveTopology primitiveTopology;
+		eDepthComparer depthComparer;
+		//TODO: add additional states
+		
+		bool bCullBackFace;
+		bool bDepthTest;
+		bool bDepthWrite;
+		bool bWireFrame;
+		float lineWidth;
+	};
 
 	class IRenderState
 	{
@@ -12,9 +55,11 @@ namespace kepler {
 		static std::shared_ptr<IRenderState> s_pInstance;
 
 	public:
-		
+		virtual ~IRenderState() = 0;
 
+		virtual void Bind() = 0;
+		virtual void GetStateDescription() = 0;
+		
 		static std::shared_ptr<IRenderState> Create();
 	};
-
 }
