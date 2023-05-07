79dce8106fa2467c650a26444cec2ef00cccc0d5
Im Yongsik <lvhi7121@gmail.com>
2023-05-07 22:24:57 +0900

    fix: setup batch file에 telescope 사양 추가

--- setup.bat
+++ setup.bat
@@ -4,8 +4,16 @@ rem copy keplermath
 copy .\Kepler\Dependencies\KeplerMath\lib\KeplerMath.dll .\bin\Debug-x64\Client
 copy .\Kepler\Dependencies\KeplerMath\lib\KeplerMath.dll .\bin\Release-x64\Client
 
+copy .\Kepler\Dependencies\KeplerMath\lib\KeplerMath.dll .\bin\Debug-x64\Telescope
+copy .\Kepler\Dependencies\KeplerMath\lib\KeplerMath.dll .\bin\Release-x64\Telescope
+
 rem copy fmod
 copy .\Kepler\Dependencies\fmod\lib\x64\fmod.dll .\bin\Debug-x64\Client
 copy .\Kepler\Dependencies\fmod\lib\x64\fmodL.dll .\bin\Debug-x64\Client
 copy .\Kepler\Dependencies\fmod\lib\x64\fmod.dll .\bin\Release-x64\Client
 copy .\Kepler\Dependencies\fmod\lib\x64\fmodL.dll .\bin\Release-x64\Client
+
+copy .\Kepler\Dependencies\fmod\lib\x64\fmod.dll .\bin\Debug-x64\Telescope
+copy .\Kepler\Dependencies\fmod\lib\x64\fmodL.dll .\bin\Debug-x64\Telescope
+copy .\Kepler\Dependencies\fmod\lib\x64\fmod.dll .\bin\Release-x64\Telescope
+copy .\Kepler\Dependencies\fmod\lib\x64\fmodL.dll .\bin\Release-x64\Telescope
